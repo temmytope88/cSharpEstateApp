a5f8ee502f2bd897d95b1c779727f4dddaf1e819
temmytope88 <fakiletemitope@gmail.com>
2020-05-29 04:14:26 +0100

    add Entities

--- src/Estateapp.Data/DBContext/AuthenticationDBcontext/AuthenticationDBContext.cs
+++ src/Estateapp.Data/DBContext/AuthenticationDBcontext/AuthenticationDBContext.cs
@@ -1,9 +1,17 @@
 
 
+using Estate.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 namespace Estateapp.Data.DBContext.AuthenticationDBContext
 {
-  public class AuthenticationDBContext: IdentityDBContext
+  public class AuthenticationDBContext: IdentityDbContext<ApplicationUser> 
   {
-
+    public AuthenticationDBContext(DbContextOptions<AuthenticationDBContext> options)
+       :base(options)
+    {
+      
+    }
   }
 }
---
+++ src/Estateapp.Data/Entities/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Estate.Data.Entities
+{
+    public class ApplicationUser: IdentityUser
+    {
+        
+    }
+}
