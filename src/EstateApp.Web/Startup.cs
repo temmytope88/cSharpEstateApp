0f068f26146692cb9d2be58035d4999d79f5990e
temmytope88 <fakiletemitope@gmail.com>
2020-06-04 19:53:23 +0100

    modify startup.cs

--- src/EstateApp.Web/Startup.cs
+++ src/EstateApp.Web/Startup.cs
@@ -16,17 +16,13 @@ namespace EstateApp.Web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
+    
     public Startup(IConfiguration configuration) 
         {
           this.Configuration = configuration;
                
         }
-                public IConfiguration Configuration { get; }
+                        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
